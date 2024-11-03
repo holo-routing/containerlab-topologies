8df30f40d7bb72d2723deb455655b820d430a727
Paul Wekesa <paul1tw1@gmail.com>
2024-11-03 10:23:33 +0300

    vrrp: rename sw to br
    Rename the connecting devices based on
    holo's standards.
    
    Signed-off-by: Paul Wekesa <paul1tw1@gmail.com>

--- vrrp/frr/frr-setup.sh
+++ vrrp/frr/frr-setup.sh
@@ -1,14 +1,7 @@
 #!/bin/sh 
 
-# waiting for the daemons to start
-sleep 2
-
-
-# Wait for the daemons to start
-sleep 5
-
 if command -v /usr/lib/frr/zebra; then
-  ip link add vrrp4-51 link eth-sw1 type macvlan mode bridge
+  ip link add vrrp4-51 link eth-br1 type macvlan mode bridge
   ip link set dev vrrp4-51 address 00:00:5e:00:01:33
   ip addr add 10.0.1.5/24 dev vrrp4-51
   ip link set dev vrrp4-51 up
